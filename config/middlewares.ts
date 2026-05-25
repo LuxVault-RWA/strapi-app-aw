63202e41b5f9d2429b6df245dbc3e9ed0a93e730
heresjohnny135 <johnherold5@gmail.com>
2026-05-25 08:09:10 +0700

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,24 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'arweave.net', '*.arweave.net'],
+          'media-src': ["'self'", 'data:', 'blob:', 'arweave.net', '*.arweave.net'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
